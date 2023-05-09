38af6c0c350744ea30fab5970b8c3e2168fb32de
Zehra <zehrraayhan@gmail.com>
2023-05-10 02:31:13 +0300

    first commit

---
+++ questapp/src/main/java/com/projectapp/questapp/controllers/LikeController.java
@@ -0,0 +1,42 @@
+package com.projectapp.questapp.controllers;
+
+import com.projectapp.questapp.entities.Like;
+import com.projectapp.questapp.requests.LikeCreateRequest;
+import com.projectapp.questapp.services.LikeService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/likes")
+@AllArgsConstructor
+public class LikeController {
+    private LikeService likeService;
+
+
+    @GetMapping
+    public List<Like> getAllLikes(@RequestParam Optional<Long> userId,@RequestParam Optional<Long> postId){
+
+        return likeService.getAllLikesWithParam(userId,postId);
+    }
+
+    @PostMapping
+    public Like createOneLike(LikeCreateRequest request){
+
+        return likeService.createOneLike(request);
+    }
+
+    @GetMapping("/{likeId}")
+    public Like getOneLike(@PathVariable Long likeId){
+
+        return likeService.getOneLikeById(likeId);
+    }
+    @DeleteMapping("/{likeId}")
+    public void deleteOneLike(@PathVariable Long likeId){
+        likeService.deleteOneLikeById(likeId);
+    }
+
+
+}
